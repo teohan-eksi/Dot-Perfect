a6ddfa3f5f45ab2557ed7d8e511ec9577fcb9113
Teohan Eksi <teohan.eksi@gmail.com>
2021-09-19 22:08:34 +0300

    show dot switch has been initialized.

--- app/src/main/java/com/sweetsour/dotperfect/MainActivity.kt
+++ app/src/main/java/com/sweetsour/dotperfect/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val showDotSwitch: SwitchCompat = findViewById(R.id.showDotSwitch)
-
+        //TODO: show dot as a floating activity
         showDotSwitch.setOnCheckedChangeListener{_ , isChecked ->
             if(isChecked){
                 Log.d("MainActivity", "showDotSwitch is checked")
