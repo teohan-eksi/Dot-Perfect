df165d2b32046a6224782e08777a7d56f9796306
Teohan Eksi <teohan.eksi@gmail.com>
2021-09-20 22:55:41 +0300

    updated main activity to start the service

--- app/src/main/java/com/sweetsour/dotperfect/MainActivity.kt
+++ app/src/main/java/com/sweetsour/dotperfect/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.sweetsour.dotperfect
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val showDotSwitch: SwitchCompat = findViewById(R.id.showDotSwitch)
-        //TODO: show dot as a floating activity
         showDotSwitch.setOnCheckedChangeListener{_ , isChecked ->
             if(isChecked){
                 Log.d("MainActivity", "showDotSwitch is checked")
+                //create an intent to start the service
+                val startServiceIntent = Intent(applicationContext, DotService::class.java )
+                startService(startServiceIntent)
             }else{
                 Log.d("MainActivity", "showDotSwitch is not checked")
+                //stopSelf()
+                val isServiceStopped = stopService(Intent(applicationContext, DotService::class.java))
+                if(isServiceStopped){
+                    Log.d(TAG, "service stopped")
+                }
             }
         }
     }
