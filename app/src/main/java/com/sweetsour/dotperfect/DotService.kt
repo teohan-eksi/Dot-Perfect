6b6551a71cfe3985f30888be0844580134da04ff
Teohan Eksi <teohan.eksi@gmail.com>
2021-09-20 22:54:09 +0300

    added service class

---
+++ app/src/main/java/com/sweetsour/dotperfect/DotService.kt
@@ -0,0 +1,64 @@
+package com.sweetsour.dotperfect
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+
+class DotService : Service() {
+    private val TAG = "DotService"
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        //return super.onStartCommand(intent, flags, startId)
+        Log.d(TAG, "Intent: $intent, flags: $flags, startId: $startId")
+
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "DotService onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "DotService onDestroy")
+        //stopSelf()
+    }
+/*
+    private fun createNotification(): Notification {
+        val notificationChannelId = "myNotif"
+        // depending on the Android API that we're dealing with we will have
+        // to use a specific method to create the notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                    NotificationManager
+            val channel = NotificationChannel(
+                notificationChannelId,
+                "Endless Service notifications channel",
+                NotificationManager.IMPORTANCE_HIGH
+            ).let {
+                it.description = "Endless Service channel"
+                it.enableLights(true)
+                it.lightColor = Color.RED
+                it.enableVibration(true)
+                it.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+                it
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(this, 0, notificationIntent, 0)
+        }
+    }
+ */
+
+}
