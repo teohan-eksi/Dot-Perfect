e5da759d8c8a95d8faf5b7078977cff272248e78
Teohan Eksi <teohan.eksi@gmail.com>
2021-09-30 16:28:58 +0300

    Now, dot shows on apps and home screen and touches go into right places

--- app/src/main/java/com/sweetsour/dotperfect/DotService.kt
+++ app/src/main/java/com/sweetsour/dotperfect/DotService.kt
@@ -4,15 +4,20 @@ import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.PixelFormat
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.WindowManager
+import android.widget.Button
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class DotService : Service() {
     private val TAG = "DotService"
-    private lateinit var dotIntent: Intent
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         //return super.onStartCommand(intent, flags, startId)
@@ -29,7 +34,45 @@ class DotService : Service() {
         super.onCreate()
         Log.d(TAG, "onCreate")
 
-        dotIntent = Intent(applicationContext, DotActivity::class.java)
+        val windowManager = applicationContext.getSystemService(Context.WINDOW_SERVICE)
+                as WindowManager
+        val layoutInflater = baseContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+                as LayoutInflater
+
+        val dotViewGroup = layoutInflater.inflate(R.layout.activity_dot, null)
+
+        var layoutType: Int
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            layoutType = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        }else{
+            layoutType = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY
+        }
+
+        val dotLayoutParams = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            layoutType,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+            PixelFormat.TRANSLUCENT
+        )
+
+        dotLayoutParams.gravity = Gravity.CENTER
+        dotLayoutParams.x = 0
+        dotLayoutParams.y = 0
+
+        windowManager.addView(dotViewGroup, dotLayoutParams)
+
+        //dot button
+        val dotPerfect: Button = dotViewGroup.findViewById(R.id.dotPerfect)
+        dotPerfect.setOnClickListener {
+            Log.d(TAG, "dot perfect tap")
+        }
+        dotPerfect.setBackgroundColor(Color.argb(150, 0, 51, 53))
+        //Color.parseColor("#AARRGGBB") also works
+
+        /*
         dotIntent.flags =
             //If a task is already running for the activity you are now starting,
             //  that task is brought to the foreground with its last state restored
@@ -46,21 +89,21 @@ class DotService : Service() {
             //FLAG_ACTIVITY_NO_HISTORY -> the new activity is not kept in the history stack.
             //  As soon as the user navigates away from it, the activity is finished.
         dotIntent.putExtra("isFinishActivity", false)
-
-        val mRunnable = Runnable {
-            startActivity(dotIntent)
-        }
-        Thread(mRunnable).start()
+        */
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy")
-        //the switch is off, so the service will be closed and at the same time you should close
-        //the dot activity.
-        dotIntent.putExtra("isFinishActivity", true)
-        startActivity(dotIntent)
         stopSelf()
     }
 }
+
+//this works... Since this activity extends AppCompatActivity which is a base class for
+//activities that wish to use some of the newer platform features on older Android devices,
+//above approaches don't work on it.
+//Use "support" methods in AppCompat activities.
+//this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+//window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
